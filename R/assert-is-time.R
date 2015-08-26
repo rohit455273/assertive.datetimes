396a893c793c13a2bc3aa7c1c171fa6c7e19c13e
richierocks <richierocks@gmail.com>
2015-08-26 20:49:59 +0300

    added is_after, is_before

--- R/assert-is-time.R
+++ R/assert-is-time.R
@@ -2,14 +2,76 @@
 
 #' @rdname is_in_past
 #' @export
+assert_all_are_after <- function(x, y, na_ignore = FALSE, 
+  severity = getOption("assertive.severity", "stop"))
+{                                                     
+  msg <- gettextf("%s are not all after %s.", get_name_in_parent(x), format(y))
+  assert_engine(
+    is_after,
+    x, 
+    msg = msg, 
+    na_ignore = na_ignore,
+    severity = severity
+  ) 
+}
+
+#' @rdname is_in_past
+#' @export
+assert_any_are_after <- function(x, y, na_ignore = FALSE, 
+  severity = getOption("assertive.severity", "stop"))
+{                                                     
+  msg <- gettextf("%s are all before %s.", get_name_in_parent(x), format(y))
+  assert_engine(
+    is_after,
+    x, 
+    msg = msg, 
+    what = "any",
+    na_ignore = na_ignore,
+    severity = severity
+  )   
+}
+ 
+#' @rdname is_in_past
+#' @export
+assert_all_are_before <- function(x, y, na_ignore = FALSE, 
+  severity = getOption("assertive.severity", "stop"))
+{                                                     
+  msg <- gettextf("%s are not all before %s.", get_name_in_parent(x), format(y))
+  assert_engine(
+    is_before,
+    x, 
+    msg = msg, 
+    na_ignore = na_ignore,
+    severity = severity
+  ) 
+}
+
+#' @rdname is_in_past
+#' @export
+assert_any_are_before <- function(x, y, na_ignore = FALSE, 
+  severity = getOption("assertive.severity", "stop"))
+{                                                     
+  msg <- gettextf("%s are all after %s.", get_name_in_parent(x), format(y))
+  assert_engine(
+    is_before,
+    x, 
+    msg = msg, 
+    what = "any",
+    na_ignore = na_ignore,
+    severity = severity
+  )   
+}
+ 
+#' @rdname is_in_past
+#' @export
 assert_all_are_in_future <- function(x, na_ignore = FALSE, 
   severity = getOption("assertive.severity", "stop"))
 {                                                     
   msg <- gettextf("%s are not all in the future.", get_name_in_parent(x))
   assert_engine(
+    is_in_future,
     x, 
     msg = msg, 
-    what = "any",
     na_ignore = na_ignore,
     severity = severity
   ) 
