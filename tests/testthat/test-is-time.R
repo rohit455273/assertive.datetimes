bad1e905e33455ccf72330b73609e4424b9486e2
richierocks <richierocks@gmail.com>
2016-05-09 20:25:58 +0300

    updated tests

--- tests/testthat/test-is-time.R
+++ tests/testthat/test-is-time.R
@@ -2,7 +2,7 @@ test_that("test.is_in_future.then_now_soon.returns_true_in_past", {
   x <- Sys.time() + c(-1, 5, NA)
   expected <- c(FALSE, TRUE, NA)
   expect_equal(strip_attributes(actual <- is_in_future(x)), expected)
-  expect_equal(names(actual), as.character(x))
+  expect_named(actual)
   expect_equal(cause(actual), noquote(c("in past", "", "missing")))
 })
 
@@ -10,6 +10,6 @@ test_that("test.is_in_past.then_now_soon.returns_true_in_past", {
   x <- Sys.time() + c(-1, 5, NA)
   expected <- c(TRUE, FALSE, NA)
   expect_equal(strip_attributes(actual <- is_in_past(x)), expected)
-  expect_equal(names(actual), as.character(x))
+  expect_named(actual)
   expect_equal(cause(actual), noquote(c("", "in future", "missing")))
 }) 
